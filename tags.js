cc05fb5f3d4054a7d82849c2e132fe6678d3bafb
Kannan Duraisamy <currentkannan@gmail.com>
2021-07-19 22:58:10 +0530

    Add files via upload

---
+++ tags.js
@@ -0,0 +1,38 @@
+        let option1=document.getElementById("select1");
+        let option2=document.getElementById("select2");
+        let option3=document.getElementById("select3");
+        let option4=document.getElementById("select4");
+        function show1(){
+            option1.style.display="inline-block";
+
+        }
+        function show2(){
+            option2.style.display="inline-block";
+
+        }
+        function show3(){
+            option3.style.display="inline-block";
+
+        }
+        function show4(){
+            option4.style.display="inline-block";
+
+        }
+     
+        function hide1(){
+            option1.style.display="none";
+
+        }
+        function hide2(){
+            option2.style.display="none";
+
+        }
+        function hide3(){
+            option3.style.display="none";
+
+        }
+        function hide4(){
+            option4.style.display="none";
+
+        }
+       
